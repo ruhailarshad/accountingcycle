704a5fcbcada61b2f94d36656023ab5e624e7c4d
SiddiquiMonis401 <SiddiquiMonis401>
2021-08-30 03:47:10 +0500

    final frontend

--- src/services/ClosingEntry.js
+++ src/services/ClosingEntry.js
@@ -4,5 +4,5 @@ import { BASE_URL } from './BaseURL';
 const postClosingEntry = `${BASE_URL.local}/closing-entry/closing`;
 
 export const postClosingEntries = () => {
-  return axios.get(postClosingEntry, {}, {});
+  return axios.post(postClosingEntry, {}, {});
 };
--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,11 @@
 import { postGeneralEntry, getGeneralEntry } from './GeneralJounral';
-import {getTrialBalanceSheet} from './TrialBalanceSheet';
-import { getFinancialStatement } from "./FinancialStatement";
-
+import { getTrialBalanceSheet } from './TrialBalanceSheet';
+import { getFinancialStatement } from './FinancialStatement';
+import { postClosingEntries } from './ClosingEntry';
 export const services = {
-    postGeneralEntry,
-    getGeneralEntry,
-    getTrialBalanceSheet,
-    getFinancialStatement,
+  postGeneralEntry,
+  getGeneralEntry,
+  getTrialBalanceSheet,
+  getFinancialStatement,
+  postClosingEntries,
 };
